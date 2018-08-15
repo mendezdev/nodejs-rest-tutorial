dbaa43994e1a778abd4e45cd9752c0a2f92cdf30
Pablo Mendez <mendez.developer@gmail.com>
2018-08-15 15:53:05 -0300

    added CORS error handler and prod/orders routes

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  // this example may be for some specific url
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:4200')
+  res.header('Access-Control-Allow-Origin', '*');
+  // could be all, passing *
+  // res.header('Access-Control-Allow-Headers', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-Width, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+})
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
